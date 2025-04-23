5e54b53e38c008abccc39ac58401116d414d2931
trihung987 <51873366+trihung987@users.noreply.github.com>
2025-04-23 14:49:20 +0700

    fix course instructor for new course

--- types/instructor-course.ts
+++ types/instructor-course.ts
@@ -1,10 +1,23 @@
 export interface CourseType {
-    id: string;
-    title: string;
-    students: number;
-    rating: number;
-    income: number;
-    lastUpdated: string;
-    published: boolean;
-    thumbnail: string;
+  id: string;
+  title: string;
+  students: number;
+  rating: number;
+  income: number;
+  lastUpdated: string;
+  published: boolean;
+  thumbnail: string;
 }
+
+export type CourseInstructorDto = {
+  id: number;
+  userId: string;
+  courseId: string;
+  isOwner: Boolean;
+};
+
+export type CourseInstructorDtoRequest = {
+  userId: string;
+  courseId: string;
+  isOwner: Boolean;
+};
