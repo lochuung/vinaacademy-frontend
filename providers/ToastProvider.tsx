bbcad6bafb5428181315f8881cd89b7f83fde0a2
Nguyen Huu Loc <locn562836@gmail.com>
2025-05-11 16:07:22 +0700

    Refactor ToastProvider to remove mounted state and always render ToastContainer

--- providers/ToastProvider.tsx
+++ providers/ToastProvider.tsx
@@ -3,37 +3,27 @@
 import "react-toastify/dist/ReactToastify.css";
 import "../app/globals.css";
 import { Bounce, ToastContainer } from "react-toastify";
-import { useEffect, useState } from "react";
 
 interface ToastProviderProps {
     children: React.ReactNode;
 }
 
 export default function ToastProvider({ children }: ToastProviderProps) {
-    const [isMounted, setIsMounted] = useState(false);
-
-    useEffect(() => {
-        setIsMounted(true);
-    }, []);
 
     return (
         <>
             {children}
-            {isMounted && (
-                <ToastContainer
-                    position="top-center"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick={false}
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                    transition={Bounce}
-                />
-            )}
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={true}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                draggable
+                theme="light"
+                transition={Bounce}
+            />
         </>
     );
 }
