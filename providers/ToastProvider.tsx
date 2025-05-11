91c323f6288e0b4460abeeedf94dc55efdc80a8e
Nguyen Huu Loc <locn562836@gmail.com>
2025-05-11 15:56:38 +0700

    Add mounted state to ToastProvider for conditional rendering of ToastContainer

--- providers/ToastProvider.tsx
+++ providers/ToastProvider.tsx
@@ -3,29 +3,37 @@
 import "react-toastify/dist/ReactToastify.css";
 import "../app/globals.css";
 import { Bounce, ToastContainer } from "react-toastify";
+import { useEffect, useState } from "react";
 
 interface ToastProviderProps {
     children: React.ReactNode;
 }
 
 export default function ToastProvider({ children }: ToastProviderProps) {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
 
     return (
         <>
             {children}
-            <ToastContainer
-                position="top-center"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-                transition={Bounce}
-            />
+            {isMounted && (
+                <ToastContainer
+                    position="top-center"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                    transition={Bounce}
+                />
+            )}
         </>
     );
 }
